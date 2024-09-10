0451c21401211039e347f11aae61c9604cd891e7
vincevill599 <vincevincevillvince@gmail.com>
2024-09-10 15:25:47 +0800

    Added collapsible effect to index's services

--- js/header.js
+++ js/header.js
@@ -1,10 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const burgerButton = document.querySelector('.navbar-toggler');
     const navbarCollapse = document.querySelector('.navbar-collapse');
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link'); // Select all the links
-    
+
+    var section = document.getElementById("service-col");
+    var button = document.getElementById("expander-button");
+    var gradient = document.getElementById("serviceback");
+    button.innerHTML = '<i class="bi bi-chevron-down"></i>';
+    gradient.style.display = "inline-block";
+    section.style.maxHeight = "85vh";
+
     // Close the navbar when the close button is clicked
-    document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.close-btn').addEventListener('click', function () {
         if (navbarCollapse.classList.contains('show')) {
             bootstrap.Collapse.getInstance(navbarCollapse).toggle(); // Toggle the collapse
         }
@@ -21,11 +28,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // Collapse the navbar when a link inside it is clicked
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
             if (navbarCollapse.classList.contains('show')) {
                 bootstrap.Collapse.getInstance(navbarCollapse).hide(); // Collapse the menu
             }
         });
     });
 });
+
+
+function toggleSection() {
+    var section = document.getElementById("service-col");
+    var button = document.getElementById("expander-button");
+    var gradient = document.getElementById("serviceback");
+
+    // Toggle expanded class
+    section.classList.toggle("expanded");
+
+    // Change button text
+    if (section.classList.contains("expanded")) {
+        button.innerHTML = '<i class="bi bi-chevron-up"></i>';
+        section.style.maxHeight = section.scrollHeight + "px";
+        gradient.style.opacity = "0%";
+    } else {
+        button.innerHTML = '<i class="bi bi-chevron-down"></i>';
+        gradient.style.opacity = "100%";
+        section.style.maxHeight = "85vh";
+    }
+}
